1c99e55919622c293915e3aa7ab33890836f30db
ludd50155 <ludd50155@hundsun.com>
2024-04-30 17:27:02 +0800

    use dap from neovim now

---
+++ lua/ldd/plugins/debugger.lua
@@ -0,0 +1,34 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+
+    -- As of gdb 14.1, gdb has itw own Debugger Adapter Protocol.
+    -- This feature is turned on if gdb built with python support.
+    dap.adapters.gdb = {
+      type = "executable",
+      command = "/home/lightdb/opt/local/bin/gdb",
+      -- `gdb -i dap` command starts the dap server
+      args = { "-i", "dap" }
+    }
+
+    dap.configurations.c = {
+      {
+          name = "launch",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          stopAtBeginningOfMainSubprogram = false,
+      },
+      {
+          name = "attach",
+          type = "gdb",
+          request = "attach",
+          pid = require("dap.utils").pick_process,
+          stopAtBeginningOfMainSubprogram = false,
+      },
+    }
+  end,
+}
--- lua/ldd/plugins/lsp/lspsaga.lua
+++ lua/ldd/plugins/lsp/lspsaga.lua
@@ -2,6 +2,9 @@ return {
   "nvimdev/lspsaga.nvim",
   config = function()
     require('lspsaga').setup({
+      lightbulb = {
+        enable = false,
+      },
       symbol_in_winbar = {
         in_custom = false,
         enable = true,
