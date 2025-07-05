e5502eef675317a8209154f454ae1c1758c04f0e
winter.loo <adjkldd@126.com>
2025-07-05 15:20:49 +0800

    update debugger settings

--- lua/ldd/plugins/debugger.lua
+++ lua/ldd/plugins/debugger.lua
@@ -2,127 +2,156 @@
 -- :packadd termdebug
 -- :help terminal-debug
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "leoluz/nvim-dap-go", -- Go-specific config helper
-    "ibhagwan/fzf-lua",
-  },
-  config = function()
-    local dap = require("dap")
-
-    local keymap = vim.keymap -- for conciseness
-
-    vim.fn.sign_define('DapStopped', { text = '▶', texthl='DiagnosticError', linehl='', numhl=''})
-    vim.fn.sign_define('DapBreakpoint', { text = '●', texthl='DiagnosticError', linehl='', numhl=''})
-    vim.fn.sign_define('DapBreakpointCondition', { text = '◐', texthl='DiagnosticError', linehl='', numhl=''})
-    vim.fn.sign_define('DapLogPoint', { text = '○', texthl = 'DiagnosticError' })
-
-    -- As of gdb 14.1, gdb has itw own Debugger Adapter Protocol.
-    -- This feature is turned on if gdb built with python support.
-    dap.adapters.gdb = {
-      type = "executable",
-      command = "gdb",
-      -- `gdb -i dap` command starts the dap server
-      args = { "--interpreter=dap", "--eval-command", "set print pretty on"}
-    }
-
-    dap.configurations.c = {
-      {
-        name = "launch",
-        type = "gdb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        args = function()
-          return vim.fn.input("args: ")
-        end,
-        stopAtBeginningOfMainSubprogram = false,
-      },
-      {
-        name = "attach",
-        type = "gdb",
-        request = "attach",
-        pid = require("dap.utils").pick_process,
-        stopAtBeginningOfMainSubprogram = false,
-      },
-    }
-
-    dap.configurations.cpp = dap.configurations.c
-
-    require("dap-go").setup()
-  end,
-  keys = function()
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"leoluz/nvim-dap-go", -- Go-specific config helper
+		"ibhagwan/fzf-lua",
+	},
+	config = function()
 		local dap = require("dap")
 
-    dap.savebp = function()
-      local all_bpts = require("dap.breakpoints").get()
-      if #all_bpts == 0 then
-        return
-      end
+		local keymap = vim.keymap -- for conciseness
 
-      local filename = vim.fn.stdpath("cache") .. "/gdb.breakpoints"
-      local file = io.open(filename, "w")
-      if file == nil then
-        vim.notify(string.format("cannot open file: %s", filename), vim.log.levels.ERROR)
-        return
-      end
+		vim.fn.sign_define(
+			"DapStopped",
+			{ text = "▶", texthl = "DiagnosticError", linehl = "DiagnosticError", numhl = "" }
+		)
+		vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DiagnosticError", linehl = "", numhl = "" })
+		vim.fn.sign_define(
+			"DapBreakpointCondition",
+			{ text = "◐", texthl = "DiagnosticError", linehl = "", numhl = "" }
+		)
+		vim.fn.sign_define("DapLogPoint", { text = "○", texthl = "DiagnosticError" })
 
-      for bufnr, bpts in pairs(all_bpts) do
-        for _, bp in ipairs(bpts) do
-          file:write(vim.fn.bufname(bufnr) .. ":" .. bp.line .. "\n")
-        end
-      end
-      file:close()
-      vim.notify(string.format("breakpoints saved to file %s", filename), vim.log.levels.INFO)
-    end
+		-- As of gdb 14.1, gdb has itw own Debugger Adapter Protocol.
+		-- This feature is turned on if gdb built with python support.
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			-- `gdb -i dap` command starts the dap server
+			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+		}
 
-    dap.loadbp = function()
-      local filename = vim.fn.stdpath("cache") .. "/gdb.breakpoints"
-      local file = io.open(filename, "r")
-      if file == nil then
-        vim.notify(string.format("cannot open file: %s", filename), vim.log.levels.ERROR)
-        return
-      end
+		dap.adapters.codelldb = {
+			type = "executable",
+			command = "codelldb",
+		}
 
-      breakpt = require("dap.breakpoints")
-      for line in file:lines() do
-        -- local bufnr = 0
-        -- local lnum = 1
-        local m = string.gmatch(line, "[^:]+")
-        local bpfile = m()
-        local lnum = m()
-        print("breakpoint at line " .. lnum .. " of file " .. bpfile)
-        breakpt.set({}, bufnr, lnum)
-      end
+		dap.configurations.c = {
+			{
+				name = "gdb launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				args = function()
+					return vim.fn.input("args: ")
+				end,
+				stopAtBeginningOfMainSubprogram = false,
+			},
+			{
+				name = "gdb attach",
+				type = "gdb",
+				request = "attach",
+				pid = require("dap.utils").pick_process,
+				stopAtBeginningOfMainSubprogram = false,
+			},
+			{
+				name = "lldb launch",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				args = function()
+					return vim.fn.input("args: ")
+				end,
+				stopAtBeginningOfMainSubprogram = false,
+			},
+			{
+				name = "lldb attach",
+				type = "codelldb",
+				request = "attach",
+				pid = require("dap.utils").pick_process,
+				stopAtBeginningOfMainSubprogram = false,
+			},
+		}
 
-      file:close()
-    end
+		dap.configurations.cpp = dap.configurations.c
 
-    dap.dapcli = function()
-      local tabh = vim.api.nvim_win_get_tabpage(0)
-      for _, w in ipairs(vim.api.nvim_tabpage_list_wins(tabh)) do
-        local b = vim.api.nvim_win_get_buf(w)
-        local bufname = vim.api.nvim_buf_get_name(b)
-        -- get '[dap-repl]' from '/home/ldd/some/path/[dap-repl]'
-        local fname = string.match(bufname, "/.*/(.*)")
-        if fname then
-          if vim.fn.buflisted(b) ~= 1 and fname == "[dap-repl]" then
-            -- switch to '[dap-repl]' buffer and enter 'insert' mode
-            vim.api.nvim_set_current_win(w)
-            vim.api.nvim_win_set_height(w, 10)
-            vim.api.nvim_input("i")
-            break
-          end
-        end
-      end
-    end
+		require("dap-go").setup()
+	end,
+	keys = function()
+		local dap = require("dap")
 
+		dap.savebp = function()
+			local all_bpts = require("dap.breakpoints").get()
+			if #all_bpts == 0 then
+				return
+			end
+
+			local filename = vim.fn.stdpath("cache") .. "/gdb.breakpoints"
+			local file = io.open(filename, "w")
+			if file == nil then
+				vim.notify(string.format("cannot open file: %s", filename), vim.log.levels.ERROR)
+				return
+			end
+
+			for bufnr, bpts in pairs(all_bpts) do
+				for _, bp in ipairs(bpts) do
+					file:write(vim.fn.bufname(bufnr) .. ":" .. bp.line .. "\n")
+				end
+			end
+			file:close()
+			vim.notify(string.format("breakpoints saved to file %s", filename), vim.log.levels.INFO)
+		end
+
+		dap.loadbp = function()
+			local filename = vim.fn.stdpath("cache") .. "/gdb.breakpoints"
+			local file = io.open(filename, "r")
+			if file == nil then
+				vim.notify(string.format("cannot open file: %s", filename), vim.log.levels.ERROR)
+				return
+			end
+
+			breakpt = require("dap.breakpoints")
+			for line in file:lines() do
+				-- local bufnr = 0
+				-- local lnum = 1
+				local m = string.gmatch(line, "[^:]+")
+				local bpfile = m()
+				local lnum = m()
+				print("breakpoint at line " .. lnum .. " of file " .. bpfile)
+				breakpt.set({}, bufnr, lnum)
+			end
+
+			file:close()
+		end
+
+		dap.dapcli = function()
+			local tabh = vim.api.nvim_win_get_tabpage(0)
+			for _, w in ipairs(vim.api.nvim_tabpage_list_wins(tabh)) do
+				local b = vim.api.nvim_win_get_buf(w)
+				local bufname = vim.api.nvim_buf_get_name(b)
+				-- get '[dap-repl]' from '/home/ldd/some/path/[dap-repl]'
+				local fname = string.match(bufname, "/.*/(.*)")
+				if fname then
+					if vim.fn.buflisted(b) ~= 1 and fname == "[dap-repl]" then
+						-- switch to '[dap-repl]' buffer and enter 'insert' mode
+						vim.api.nvim_set_current_win(w)
+						vim.api.nvim_win_set_height(w, 10)
+						vim.api.nvim_input("i")
+						break
+					end
+				end
+			end
+		end
 
 		return {
 			{ "<leader>dbb", dap.toggle_breakpoint, desc = "toggle breakpoint" },
 			{ "<leader>dbS", dap.savebp, desc = "Save breakpoints" },
-      { "<leader>dbL", dap.loadbp, desc = "Load breakpoints" },
+			{ "<leader>dbL", dap.loadbp, desc = "Load breakpoints" },
 			{
 				"<leader>dbl",
 				function()
@@ -132,7 +161,7 @@ return {
 				end,
 				desc = "Toggle Logpoint",
 			},
-      {
+			{
 				"<leader>dbc",
 				function()
 					vim.ui.input({ prompt = "Breakpoint condition: " }, function(input)
@@ -146,7 +175,7 @@ return {
 			{ "<leader>dj", dap.step_into, desc = "step into the function" },
 			{ "<leader>dl", dap.step_over, desc = "step over the function" },
 			{ "<leader>dk", dap.step_out, desc = "step out of the function" },
-      { "<leader>drl", dap.run_last, desc = "Run Last" },
+			{ "<leader>drl", dap.run_last, desc = "Run Last" },
 			{
 				"<leader>de",
 				function()
@@ -160,14 +189,14 @@ return {
 			{
 				"<leader>dv",
 				function()
-          require("fzf-lua").dap_variables()
+					require("fzf-lua").dap_variables()
 				end,
 				desc = "list current frame variables",
 			},
 			{
 				"<leader>df",
 				function()
-          require("fzf-lua").dap_frames()
+					require("fzf-lua").dap_frames()
 				end,
 				desc = "list frames",
 			},
