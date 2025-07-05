a904020d67e22631825e4d5fefef512df076236d
winter.loo <adjkldd@126.com>
2025-07-05 21:48:52 +0800

    add new plugin screekey

---
+++ lua/ldd/plugins/screekey.lua
@@ -0,0 +1,15 @@
+return {
+	"nvzone/showkeys",
+	enabled = false,
+	cmd = "ShowkeysToggle",
+	winopts = {
+		-- focusable = false,
+		relative = "editor",
+		style = "minimal",
+		border = "single",
+		height = 1,
+		row = 1,
+		col = 0,
+		zindex = 100,
+	},
+}
