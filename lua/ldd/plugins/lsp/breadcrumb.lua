752d647f3f52d58ca34c2d7218c58cffe9b73afd
winter.loo <adjkldd@126.com>
2025-07-14 14:09:58 +0800

    dropbar custom background not working

--- lua/ldd/plugins/lsp/breadcrumb.lua
+++ lua/ldd/plugins/lsp/breadcrumb.lua
@@ -6,10 +6,9 @@ return {
 		vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
 		vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
 
+    -- FIX: not working
     vim.api.nvim_set_hl(0, "DropBarMenuCurrentContext", {
       bg = "#44475a", -- Dracula selection gray
     })
-
-    vim.api.nvim_set_hl(0, 'DropBarFileName', { fg = '#FF0000', italic = true })
 	end,
 }
