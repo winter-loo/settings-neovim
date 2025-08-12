cf6e6bf956a320cafdad724d033d43bd48e615e1
winter.loo <winterloo@126.com>
2025-08-12 14:47:15 +0800

    configure diffview plugin

--- lua/ldd/plugins/diffview.lua
+++ lua/ldd/plugins/diffview.lua
@@ -1,3 +1,19 @@
 return {
-  "sindrets/diffview.nvim"
+  "sindrets/diffview.nvim",
+  config = function()
+    require('diffview').setup({
+      view = {
+        default = {
+          -- ┌──────────────┐
+          -- │      A       │
+          -- │              │
+          -- ├──────────────┤
+          -- │      B       │
+          -- │              │
+          -- └──────────────┘
+          layout = "diff2_vertical"
+        }
+      }
+    })
+  end
 }
