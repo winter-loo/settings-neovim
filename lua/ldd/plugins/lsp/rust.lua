de8ae2ad5c9ec35e0d166a74077c262c33a78b4a
winter.loo <winterloo@126.com>
2025-12-16 11:03:15 +0800

    configure rust analyzer

--- lua/ldd/plugins/lsp/rust.lua
+++ lua/ldd/plugins/lsp/rust.lua
@@ -1,5 +1,29 @@
 return {
   'mrcjkb/rustaceanvim',
   version = '^6', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  lazy = false,   -- This plugin is already lazy
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        default_settings = {
+          ['rust-analyzer'] = {
+            cargo = {
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+              allFeatures = true,
+            },
+            procMacro = {
+              enable = true,
+            },
+            inlayHints = {
+              lifetimeElisionHints = {
+                enable = true,
+                useParameterNames = true,
+              },
+            },
+          },
+        },
+      },
+    }
+  end
 }
